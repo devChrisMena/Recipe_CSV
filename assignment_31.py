6727a43163307f0ffbd5a42761097216a80cbed6
Christopher Mena <christophermena9013@gmail.com>
2019-10-18 23:35:52 -0400

    Fix script

--- assignment_31.py
+++ assignment_31.py
@@ -1,7 +1,7 @@
 #Name: Christopher Mena
 #Email: christopher.mena66@myhunter.cuny.edu
 #Date: October 18, 2019
-#Read and display csv user input
+#Read, modify and display csv user input
 
 #Import libaries 
 import pandas as pd
@@ -11,8 +11,9 @@ look_recipe = input("Enter recipe name: ")
 
 #Read csv file
 recipe = pd.read_csv(look_recipe)
-
+recipe["Amount"] = recipe["Amount"] * 2
 
 #Output
+
 print("Double your recipe is:")
 print(recipe)
